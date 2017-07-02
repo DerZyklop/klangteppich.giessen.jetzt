7a8cff7b0ae0df3c3fb9c1eb2e18e352e05fcd80
Nils Neumann <nils@dr-plano.de>
2017-07-02 22:57:55 +0200

    added accounts

---
+++ site/accounts/derzyklop.php
@@ -0,0 +1,14 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: derzyklop
+password: >
+  $2a$10$RpIWbkjcPbGFmEvb93WGJeyaR2mwv5hQ9S662HAkuOSouTLHcZyJm
+email: mail@der-zyklop.de
+language: de
+role: admin
+history:
+  - blog
+  - anfahrt
+  - freunde/project-d
+  - freunde/project-c
+  - freunde/project-e
---
+++ site/accounts/michel.php
@@ -0,0 +1,16 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: michel
+password: >
+  $2a$10$5pBbScu09C/LuHuJ/1GzV.Nj4LlosQzBJkPbpwWMDA1l2E7CH5rkG
+email: mail@der-zyklop.de
+language: de
+role: admin
+history:
+  - blog
+  - anfahrt
+  - freunde/project-d
+  - freunde/project-c
+  - freunde/project-e
+firstname: ""
+lastname: ""
