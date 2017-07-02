a17ced515fca5e24f7e9c047a0f803f8150c7eaf
Nils Neumann <nils@dr-plano.de>
2017-07-02 22:44:25 +0200

    anfahrt

--- site/templates/about.php
+++ site/templates/about.php
@@ -14,11 +14,11 @@
     <div class="wrap">
 
       <header>
-        <h1><?= $page->title()->html() ?></h1>
-        <div class="intro text">
+        <!-- <h1><?= $page->title()->html() ?></h1> -->
+        <!-- <div class="intro text">
           <?= $page->intro()->kirbytext() ?>
-        </div>
-        <hr />
+        </div> -->
+        <!-- <hr /> -->
       </header>
 
       <div class="text">
