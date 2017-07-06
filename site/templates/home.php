1d0ad1c591cef75cdaec46e442fc04f1d80cc96c
Nils Neumann <nils@dr-plano.de>
2017-07-06 09:54:07 +0200

    link zu facebook

--- site/templates/home.php
+++ site/templates/home.php
@@ -1,7 +1,7 @@
 <?php snippet('header') ?>
 
   <?php if($image = $page->images()->sortBy('sort', 'asc')->first()): $thumb = $image; ?>
-    <div style="max-width: 1224px; margin: auto;">
+    <div style="max-width: 1224px; margin: auto; padding-bottom: 1.5rem;">
       <div style="margin: 0 1.5em;">
         <div style="padding: 0.2rem; background: #fff;">
           <img src="<?= $thumb->url() ?>" alt="Thumbnail for <?= $page->title()->html() ?>" class="showcase-image" />
